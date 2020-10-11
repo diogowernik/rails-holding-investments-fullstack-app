d8406045c0c75603dfe758c75c89c148899a1fd7
Diogo Wernik <diogowernik@gmail.com>
2020-10-11 02:39:49 +0000

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_224336) do
+ActiveRecord::Schema.define(version: 2020_10_10_040131) do
 
   create_table "atividades", force: :cascade do |t|
     t.string "codigo"
